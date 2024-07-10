1d0d6bddc3cb38537bed5ccd74df196e5d9e7bb9
Lohan Costa <lohanbcosta@gmail.com>
2024-07-09 23:28:15 -0300

    manipulação do DOM usando a biblioteca jQuery

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 $(document).ready(function () {
   $("header button").click(function () {
-    alert("Expandir formulário");
+    $("form").slideDown();
+  });
+
+  $("#botaoCancela").click(function () {
+    $("form").slideUp();
   });
 
   $("form").on("submit", function (e) {
-    console.log("submit");
     e.preventDefault();
+    const enderecoDaNovaImagem = $("#enderecoImagemNova").val();
+    const novoItem = $("<li style='display: none'></li>");
+    $(`<img src="${enderecoDaNovaImagem}"/>`).appendTo(novoItem);
+    $(`< class="overlay-imagem-link"/>
+      <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+        Ver imagem em tamanho real
+      </a>
+      </div>`).appendTo(novoItem);
+    $(novoItem).appendTo("ul");
+    $(novoItem).fadeIn(1000);
+    $("enderecoImagemNova").val("");
   });
 });
